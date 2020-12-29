5fd3666e55ceb112e6fe5ef2e65ffca5390393cf
Mateusz Frąckowiak <65500997+xfredeq@users.noreply.github.com>
2020-12-29 13:48:37 +0100

    2.1

---
+++ src/com/company/Pirate_Wars.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.awt.EventQueue;
+
+public class Pirate_Wars
+{
+
+    public static void main (String[] args)
+    {
+        EventQueue.invokeLater(Window::new);
+        System.out.println("HELLO WORLD\n");
+    }
+
+}
--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -1,15 +1,26 @@
 package com.company;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
-public class Window extends JFrame
-{
-    public Window()
+public class Window extends JFrame {
+    public Window ( )
     {
         super("Pirate Wars");
+
+        setFrame();
+    }
+
+    private void setFrame ( )
+    {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1280, 720);
+        Dimension windowSize = getSize();
+        GraphicsEnvironment screen = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        Point center = screen.getCenterPoint();
+        int x = center.x - windowSize.width / 2;
+        int y = center.y - windowSize.height / 2;
+        setLocation(x, y);
         setVisible(true);
-        setLocationRelativeTo(null);
-        setSize(640,480);
     }
 }
